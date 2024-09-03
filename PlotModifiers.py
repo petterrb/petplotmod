c2c9d5ed6fe979b669bb81c31776b2163a156c86
petterrb <petter.benonisen@gmail.com>
2024-09-03 15:42:13 +0200

    Bug fix

--- PlotModifiers.py
+++ PlotModifiers.py
@@ -3,8 +3,9 @@ from matplotlib import use
 import numpy as np
 
 class PlotModifiers:
-    def __init__(self, xlabel=None, ylabel=None, title=None, secondary_title=None, xlim=None, ylim=None, grid=False, legend=False,
-                 title_align=None, format_origin=False, scientific_x=False, scientific_y=False):
+    def __init__(self, xlabel=None, ylabel=None, title=None, secondary_title=None, xlim=None, ylim=None, grid=False,
+                 legend=False,title_align=None, format_origin=False, scale_x=None, scale_y=None, scientific_x=False,
+                 scientific_y=False):
         self.xlabel = xlabel
         self.ylabel = ylabel
         self.title = title
